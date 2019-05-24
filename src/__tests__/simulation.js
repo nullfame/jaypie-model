6fef2b0d107e43ac80a53ab594fa3309058133b3
Adam Finlayson <amf@adamfinlayson.com>
2019-05-24 17:26:45 -0500

    :construction::construction::construction:

---
+++ src/__tests__/simulation.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+class AnimalModel {
+  // constructor(params) {}
+}
+
+// eslint-disable-next-line no-unused-vars
+function animalController(event) {
+  //
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export const animalHandler = event => animalController(event);
