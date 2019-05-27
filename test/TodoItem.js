bf245c01bd263d825c3d494cfebc646c22202d8d
Adam Finlayson <amf@adamfinlayson.com>
2019-05-27 11:48:55 -0500

    :construction: Extend Model

--- test/TodoItem.js
+++ test/TodoItem.js
@@ -1,5 +1,8 @@
-export default class TodoItem {
+import { Model } from "..";
+
+export default class TodoItem extends Model {
   constructor({ text = null, done = false } = {}) {
+    super();
     this.text = text;
     this.done = done;
   }
