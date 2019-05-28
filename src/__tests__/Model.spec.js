600f1730401e6ac6a64b6f92fdf0084c3d71fb68
Adam Finlayson <amf@adamfinlayson.com>
2019-05-27 20:57:37 -0500

    :bulb: Clarify test comments

--- src/__tests__/Model.spec.js
+++ src/__tests__/Model.spec.js
@@ -10,7 +10,7 @@ describe("TodoItem from Model.new", () => {
         { name: "done", default: false }
       ]);
     });
-    it("should have a default state", () => {
+    it("should allow default values", () => {
       const item = new TestModel();
       expect(item.text).toEqual(null);
       expect(item.done).toEqual(false);
@@ -21,12 +21,12 @@ describe("TodoItem from Model.new", () => {
     beforeEach(() => {
       TestModel = Model.new(["text", "done"]);
     });
-    it("should have a default state", () => {
+    it("should have a default state of undefined", () => {
       const item = new TestModel();
       expect(item.text).toEqual(undefined);
       expect(item.done).toEqual(undefined);
     });
-    it("should accept a text and done state", () => {
+    it("should accept initial values", () => {
       const item = new TestModel({ text: "My Item", done: false });
       expect(item.text).toEqual("My Item");
       expect(item.done).toEqual(false);
