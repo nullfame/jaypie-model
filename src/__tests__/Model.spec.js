1ca8a2bb3fe30ffacea8e73c2915541b800676b5
Adam Finlayson <amf@adamfinlayson.com>
2019-05-27 20:53:59 -0500

    :construction_site: Move model test to main src/

--- src/__tests__/Model.spec.js
+++ src/__tests__/Model.spec.js
@@ -1,24 +1,33 @@
-import { Model } from "../..";
-import { TodoItem } from "../models";
+import Model from "../Model";
 
 describe("TodoItem from Model.new", () => {
+  let TestModel;
+
   describe("Using configured attributes", () => {
+    beforeEach(() => {
+      TestModel = Model.new([
+        { name: "text", default: null },
+        { name: "done", default: false }
+      ]);
+    });
     it("should have a default state", () => {
-      const item = new TodoItem();
+      const item = new TestModel();
       expect(item.text).toEqual(null);
       expect(item.done).toEqual(false);
     });
   });
 
   describe("Using attributes array of strings (becoming untyped variables, defaulting to undefined)", () => {
-    const TodoItemStrings = Model.new(["text", "done"]);
+    beforeEach(() => {
+      TestModel = Model.new(["text", "done"]);
+    });
     it("should have a default state", () => {
-      const item = new TodoItemStrings();
+      const item = new TestModel();
       expect(item.text).toEqual(undefined);
       expect(item.done).toEqual(undefined);
     });
     it("should accept a text and done state", () => {
-      const item = new TodoItemStrings({ text: "My Item", done: false });
+      const item = new TestModel({ text: "My Item", done: false });
       expect(item.text).toEqual("My Item");
       expect(item.done).toEqual(false);
     });
