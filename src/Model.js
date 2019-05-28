35c7043dbc09476df62d82be88acf98c840f0000
Adam Finlayson <amf@adamfinlayson.com>
2019-05-27 21:55:36 -0500

    :bulb: Clarifying comments

--- src/Model.js
+++ src/Model.js
@@ -7,11 +7,14 @@ export default {
     const Model = class {
       constructor(initialValues = {}) {
         attributes.forEach(attribute => {
+          // Make sure the attribute is defined as a string or an object
           switch (typeof attribute) {
+            // When its a string, cast it into an object with a name
             case "string":
               // eslint-disable-next-line no-param-reassign
               attribute = { name: attribute };
               break;
+            // When its an object, make sure it has a name
             case "object":
               if (!("name" in attribute)) {
                 throw Error(
@@ -19,30 +22,35 @@ export default {
                 );
               }
               break;
+            // Otherwise, error out
             default:
               throw Error(
                 "Jaypie: Model.new: unexpected attribute type (invalid configuration)"
               );
           }
 
+          // Set the default value of this attribute or null
           if ("default" in attribute) {
             this[attribute.name] = attribute.default;
           } else {
             this[attribute.name] = undefined;
           }
+
+          // If an initial value was passed, use it
           if (attribute.name in initialValues) {
             this[attribute.name] = initialValues[attribute.name];
           }
         });
-      }
-    };
+      } // END Model.new class constructor
+    }; // END Model.new class
 
     return Model;
-  },
+  }, // END Model.new
+
   /** Defines data types models attributes may have */
   type: {
     any: "ANY",
     boolean: "BOOLEAN",
     string: "STRING"
-  }
+  } // END Model.type
 };
